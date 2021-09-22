a2fab879ac57a208eaa1dc241d2fa80a601d0dc0
xTeKc <bcd3v@pm.me>
2021-09-21 23:17:08 -0400

    refactor process token func

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use html5ever::tokenizer::{
     BufferQueue,
     Tag,
-    Tagkind,
+    TagKind,
     TagToken,
     Token,
     TokenSink,
@@ -23,13 +23,15 @@ struct LinkQueue {
 impl TokenSink for &mut LinkQueue {
     type Handle = ();
 
-    fn process_token(
-        &mut self, 
-        token: Token,
-        line_number: u64
-    ) -> TokenSinkResult<Self::Handle> {
-        
+    fn process_token(&mut self, token: Token,line_number: u64) -> match Token {
+        TagToken(ref tag @ Tag {
+            king: TagKind::StartTag,
+            ..
+        },
+    ) => {}
+    _ => {}
     }
+    TokenSinkResult::Continue
 }
 
 fn main() {
