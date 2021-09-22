a3945f9334a90d9e86a477216d68a89c4eb75742
xTeKc <bcd3v@pm.me>
2021-09-21 23:04:39 -0400

    import depends

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use html5ever::tokenizer::{
+    BufferQueue,
+    Tag,
+    Tagkind,
+    TagToken,
+    Token,
+    TokenSink,
+    TokenSinkResult,
+    Tokenizer,
+    TokenizerOpts,
+}:
+use std::borrow::Borrow;
+use url::{
+    ParseError,
+    Url
+};
 
 
 fn main() {
-    
+
 }
