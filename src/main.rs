2253dab06260253808f24a08fa23bd190981427c
xTeKc <bcd3v@pm.me>
2021-09-22 07:54:49 -0400

    refactor final

--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,29 @@ impl TokenSink for &mut LinkQueue {
     type Handle = ();
 
     // <a href="link">some text</a>
-    fn process_token(&mut self, token: Token,line_number: u64) -> match Token {
-        TagToken(ref tag @ Tag {
-            king: TagKind::StartTag,
-            ..
-        },
-    ) => {}
-        if tag.name.as_ref() == "a" {
-            for attributes in tag.attrs.iter() {
-                if attribute.name.local.as_ref() == "href" {
-                    let url_str: &[u8] = attribute.value.borrow();
-                    self.links
-                        .push(String::from_utf8_lossy(url_str).into_owned());
+    fn process_token(&mut self, token: Token, line_number: u64) -> TokenSinkResult<Self::Handle> {
+        match token {
+            TagToken(
+                ref tag @ Tag {
+                    kind: TagKind::StartTag,
+                    ..
+                },
+            ) => {
+                if tag.name.as_ref() == "a" {
+                    for attribute in tag.attrs.iter() {
+                        if attribute.name.local.as_ref() == "href" {
+                            let url_str: &[u8] = attribute.value.borrow();
+                            self.links
+                                .push(String::from_utf8_lossy(url_str).into_owned());
+                        }
+                    }
                 }
             }
+
+            _ => {}
         }
+        TokenSinkResult::Continue
     }
-    TokenSinkResult::Continue
 }
 
 pub fn get_links(url: &Url, page: String) -> Vec<Url> {
@@ -61,7 +67,7 @@ pub fn get_links(url: &Url, page: String) -> Vec<Url> {
         .links
         .iter()
         .map(|link| match Url::parse(link) {
-            Err(ParseError::RelativeUrlWithoutBase) => domain_url.join(link).unwrap()
+            Err(ParseError::RelativeUrlWithoutBase) => domain_url.join(link).unwrap(),
             Err(_) => panic!("Malformed link found: {}", link),
 
             Ok(url) => url,
@@ -107,8 +113,8 @@ async fn crawl(pages: Vec<Url>, current: u8, max: u8) -> CrawlResult {
     Ok(())
 }
 
-fn main() {
+fn main() -> CrawlResult {
     task::block_on(async {
-        box_crawl(vec![Url::parse("https://www.rust-lang.org").unwrap()], 1, 2);
-    });
+        box_crawl(vec![Url::parse("https://www.rust-lang.org").unwrap()], 1, 2).await
+    })
 }
